7d29125418c4f97899a7879767d0fcf75dcc00b3
Miriam Damian Retamozo <miriamdamianretamozo@gmail.com>
2023-08-10 00:35:39 -0500

    creacion de rutas para login y calendario

---
+++ Calendar/src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth/pages";
+import { CalendarPage } from "../calendar/pages";
+
+export const AppRouter = () => {
+
+  const authStatus = 'not-authenticated';
+
+  return (
+    <Routes>
+      {(authStatus === 'not-authenticated')
+        ? <Route path="/auth/*" element={<LoginPage/>}/>
+        :  <Route path="/*" element={<CalendarPage/>}/>
+      }
+       
+        <Route path="/*" element={<Navigate to="/auth/login"/>} />
+      
+    </Routes>
+  )
+}
